3fe32fb303f35cc52de2023d9ae10600ab8704a3
dbertella <daniele.bertella@gmail.com>
2019-09-29 23:30:42 +0100

    Add len clear methods, remove is broken, iterator it's broken too

--- hashmap/src/main.rs
+++ hashmap/src/main.rs
@@ -3,5 +3,9 @@ use hashmap::my_hash::MyHash;
 fn main() {
     let mut bla = MyHash::new();
     bla.insert("bla".to_string(), "Blabla");
-    dbg!(bla.get("bla".to_string()));
+    dbg!(bla.remove("bla".to_string()));
+
+    println!("{}", bla.len());
+    // bla.clear();
+    // dbg!(bla.get("bla".to_string()));
 }
--- hashmap/src/my_hash.rs
+++ hashmap/src/my_hash.rs
@@ -8,19 +8,27 @@ pub struct MyHash<K, T>
 }
 
 impl<K, T> MyHash<K, T>
-    where K: Hashing + Clone + PartialEq,
+    where K: Hashing + PartialEq,
           T: Clone
 {
-    pub fn new() -> Self
-    {
-        Self { inner: vec![None; 16 * BUCKET_SIZE] }
-    }
-
     fn calc_index(&self, key: &K) -> usize
     {
         key.hashing() % (self.inner.len() / BUCKET_SIZE) * BUCKET_SIZE
     }
-    
+
+    pub fn new() -> Self
+    {
+        let mut new_vec = Vec::new();
+        for _ in 0..(16 * BUCKET_SIZE)
+        {
+            new_vec.push(None)
+        }
+        Self
+        {
+            inner: new_vec
+        }
+    }
+
     /// Insert to hashmap
     /// ```
     ///     # use hashmap::my_hash::MyHash;
@@ -54,7 +62,7 @@ impl<K, T> MyHash<K, T>
     pub fn get(&self, key: K) -> Option<&T>
     {
         let index = self.calc_index(&key);
-        
+
         for i in index..index+BUCKET_SIZE
         {
             match &self.inner[i]
@@ -68,5 +76,55 @@ impl<K, T> MyHash<K, T>
             }
         }
         None
+    }
+
+    // Removes a key from the map, returning the value at
+    // the key if the key was previously in the map.
+    pub fn remove(&mut self, key: K) -> Option<&T>
+    {
+        let index = self.calc_index(&key);
+        for i in index..index+BUCKET_SIZE
+        {
+            match &self.inner[i]
+            {
+                Some((k, v)) => {
+                    if k == &key {
+                        self.inner[i] = None;
+                        return Some(v)
+                    }
+                },
+                None => return None,
+            }
+        }
+        None
+    }
+
+    // Clears the map, removing all key-value pairs.
+    // Keeps the allocated memory for reuse.
+    pub fn clear(&mut self)
+    {
+        self.inner.clear()
+    }
+
+    // Returns the number of elements in the map.
+    pub fn len(&self) -> usize
+    {
+        self.inner.len()
+    }
+}
+
+struct MyHashIter<K, T>
+{
+    count: usize
+}
+
+impl<K, T> Iterator for MyHashIter<K, T>
+{
+    type Item = (K, T);
+    fn next(&mut self) -> Self::Item
+    {
+        let count = self.count;
+        self.count += 1;
+        self.inner[count]
     }
 }
