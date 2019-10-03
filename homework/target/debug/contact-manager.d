abe3ac944ac6e9a32ecfb64379fd28f139465d85
dbertella <daniele.bertella@gmail.com>
2019-10-03 16:16:28 +0100

    Remove target

--- homework/target/debug/contact-manager.d
+++
@@ -1,1 +0,0 @@
-/Users/danielebertella/Dev/temp/contact-manager/homework/target/debug/contact-manager.dSYM: /Users/danielebertella/Dev/temp/contact-manager/homework/src/main.rs
--- homework/target/rls/debug/deps/contact_manager-fa2b3d3a0d9381ef.d
+++
@@ -1,5 +0,0 @@
-/Users/danielebertella/Dev/temp/contact-manager/target/rls/debug/deps/contact_manager-fa2b3d3a0d9381ef.rmeta: src/main.rs
-
-/Users/danielebertella/Dev/temp/contact-manager/target/rls/debug/deps/contact_manager-fa2b3d3a0d9381ef.d: src/main.rs
-
-src/main.rs:
